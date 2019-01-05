428eae8f2715b737925c017958cebae8a3cc5b58
Wesley de Souza <wesleywex@gmail.com>
2019-01-05 11:59:19 -0500

    Initial package and npm binary

---
+++ bin/muniverse.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const reason = `
+We are still waiting on instructions
+from God on how to securely connect to
+muniverse.
+
+The earliest protocol still open has
+not yet been invented, but a workaround
+might be possible soon.
+
+We were ordered to reserve this package
+name until then.
+`;
+
+console.error(reason);
+
+process.exit(1);
